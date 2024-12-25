f4672932a4c6ffb93fe4f9cbfa57b0e877c7f31d
Tamimur <tamimr008@gmail.com>
2024-12-25 21:04:06 +0600

    here i update this code for best case cinario

--- bubble_sort.cpp
+++ bubble_sort.cpp
@@ -1,21 +1,31 @@
-#include<bits/stdc++.h>
+
+// for worst case and average time complexity O(n^2) when array is not sorted
+//for best case time complexity is O(n) when array is sorted.
+
+#include <bits/stdc++.h>
 using namespace std;
 int main()
-{ 
-    int arr[]={13,46,24,52,20,9};
-    int size = sizeof(arr)/sizeof(arr[0]);
-    for(int i=0;i<size;i++){
-       int  min = i;
-        for(int j = 0;j<size-i-1;j++){
-            if(arr[j]>arr[j+1]){
-                swap(arr[j],arr[j+1]);
+{
+    int arr[] = {13, 46, 24, 52, 20, 9};
+    int size = sizeof(arr) / sizeof(arr[0]);
+    for (int i = 0; i < size; i++)
+    {
+        int didswap = 0;
+        for (int j = 0; j < size - i - 1; j++)
+        {
+            if (arr[j] > arr[j + 1])
+            {
+                swap(arr[j], arr[j + 1]);
+                didswap = 1;
             }
         }
-            for(int i=0;i<size;i++){
-        cout<<arr[i]<<" ";
-
+        if (didswap == 0)
+        {
+            break;
+        }
     }
-    cout<<endl;
+    for (int i = 0; i < size; i++)
+    {
+        cout << arr[i] << " ";
     }
-
 }
