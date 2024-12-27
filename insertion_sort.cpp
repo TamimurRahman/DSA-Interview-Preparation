cf8abc483619c55c057718ef6419394becabd48a
Tamimur <tamimr008@gmail.com>
2024-12-28 03:17:28 +0600

    This is the selection sort problem.

---
+++ insertion_sort.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{ 
+    int arr[]={14,9,15,12,6,8,13};
+    int size = sizeof(arr)/sizeof(arr[0]);
+    for (int i = 0; i < size; i++)
+    {
+        /* code */
+        int j = i;
+        while (j>0 && arr[j]<arr[j-1])
+        {
+            /* code */
+            swap(arr[j],arr[j-1]);
+            j--;
+        }
+        
+    }
+    for(int i =0;i<size;i++){
+        cout<<arr[i]<<" ";
+    }cout<<endl;
+    
+}
